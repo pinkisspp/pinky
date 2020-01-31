ae3177357a2cbc8a5dc6e8af44ddafd68267a023
pinkisspp <59817942+pinkisspp@users.noreply.github.com>
2020-01-31 20:44:35 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+$(function(){
+  //--------------ここから　script.js-----------------------
+
+
+  // ----------------main----------------------------------
+  $('.accordion').click(function() {
+    var $answer = $(this).find('.answer');
+    if($answer.hasClass('open')) {
+      $answer.removeClass('open');
+      $answer.slideUp();
+      $(this).find('span').text('+');
+    } else {
+      $answer.addClass('open'); 
+      $answer.slideDown();
+      $(this).find('span').text('-');
+    }
+  });
+
+
+
+// --------------------ここまで---------------------------
+});
